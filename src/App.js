3405931760c71adfbdca772b4e3343977d8441b1
MaseJ <mjohnson434@student.cvtc.edu>
2022-06-27 10:34:50 -0500

    Create tenzies game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,163 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Die from "./Die"
+import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+
+    // State
+    const [dice, setDice] = React.useState(allNewDice())
+    const [tenzies, setTenzies] = React.useState(false)
+    // State to track high scores
+    const [turns, setTurns] = React.useState(0)
+    const [score, SetScore] = React.useState(JSON.parse(localStorage.getItem("Score")) | [0])
+    // State to track time
+    const [seconds, setSeconds] = React.useState(0)
+    const [isActive, setIsActive] = React.useState(false)
+    const [bestTime, setBestTime] = React.useState(JSON.parse(localStorage.getItem("Time")) | [0])
+
+    React.useEffect(() => {
+        let interval = null;
+        if (isActive) {
+          interval = setInterval(() => {
+            setSeconds(seconds => seconds + 1);
+          }, 1000);
+        } else if (!isActive && seconds !== 0) {
+          clearInterval(interval);
+        }
+        console.log(seconds)
+        console.log(isActive)
+        return () => clearInterval(interval);
+    }, [isActive, seconds]);
+
+    // Hook to check for winner for every roll
+    React.useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld)
+        
+        if(isActive === false){
+            setIsActive(!isActive)
+        }
+
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die => die.value === firstValue)
+        
+        if (allHeld && allSameValue) {
+            setTenzies(true)
+
+            const highScore = JSON.parse(localStorage.getItem("Score"))
+            const currentScore = turns
+            const bestSeconds = JSON.parse(localStorage.getItem("Time"))
+            const currentTime = seconds
+
+            // If game is over (tenzies is true)
+            // Check score to highscore
+            // If highscore is null means it is the first play through
+            // and we will just set the current score to highscore
+            if(highScore !== null){
+                if(currentScore < highScore){
+                    SetScore(currentScore)
+                    localStorage.setItem("Score", JSON.stringify(currentScore))
+                }
+            } else {
+                localStorage.setItem("Score", JSON.stringify(currentScore))
+                SetScore(currentScore)
+            }
+
+
+            // Doing same as above but with the time
+            if(bestSeconds !== null){
+                if(currentTime < bestSeconds){
+                    setBestTime(currentTime)
+                    localStorage.setItem("Time", JSON.stringify(currentTime))
+                }
+            } else {
+                setBestTime(currentTime)
+                localStorage.setItem("Time", JSON.stringify(currentTime))
+            }
+
+            setSeconds(0)
+            setIsActive(!isActive)
+
+        }
+
+    }, [dice])
+
+
+    // Functions
+    
+    // Used to create new set of dice...
+    // Start of game or when roll is clicked
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(generateNewDie())
+        }
+        return newDice
+    }
+    
+    function rollDice() {
+        if(!tenzies) {
+            setDice(oldDice => oldDice.map(die => {
+                return die.isHeld ? 
+                    die :
+                    generateNewDie()
+            }))
+            setTurns(turns + 1)
+        } else {
+            setTenzies(false)
+            setDice(allNewDice())
+            setTurns(0)
+        }
+    }
+    
+    function holdDice(id) {
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
+    }
+    
+    // Variable to pass all dice and values to Die.js
+    const diceElements = dice.map(die => (
+        <Die 
+            key={die.id} 
+            value={die.value} 
+            isHeld={die.isHeld} 
+            holdDice={() => holdDice(die.id)}
+        />
+    ))
+    
+    return (
+        <main>
+            {tenzies ? <Confetti /> : ""}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. 
+            Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button 
+                className="roll-dice" 
+                onClick={rollDice}
+            >
+                {tenzies ? "New Game" : "Roll"}
+            </button>
+            <div className="score--container">
+                
+                <p className="best--time">Best Time: {bestTime} Seconds</p>
+                <p className="best--score">Current Score: {turns} Turns</p>
+                <p className="best--score">Best Score: {score} Turns</p>
+            </div>
+        </main>
+    )
 }
-
-export default App;
---
+++ src/Die.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Die(props) {
+
+    // Styles that render if die is clicked or not
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+    return (
+        <div 
+            className="die-face" 
+            style={styles}
+            onClick={props.holdDice}
+        >
+            <h2 className="die-num">{props.value}</h2>
+        </div>
+    )
+}
